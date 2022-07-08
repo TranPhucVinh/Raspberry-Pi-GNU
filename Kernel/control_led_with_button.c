6386be828ad29bda44afdbca3723452a56a48dbf
Tran Phuc Vinh <tranphucvinh96@gmail.com>
2022-07-08 23:12:55 +0700

    Update several functions to be compatible with all Raspbian Kernel header

--- Kernel/control_led_with_button.c
+++ Kernel/control_led_with_button.c
@@ -10,17 +10,26 @@
 
 MODULE_LICENSE("GPL");
 
-int 	led_status = 0;
 struct 	timer_list kernel_timer;
 
 void timer_callback(struct timer_list* tl){
-	gpio_direction_output(LED, gpio_direction_input(BUTTON));
+	gpio_direction_output(LED, gpio_get_value(BUTTON));
 	mod_timer(&kernel_timer, jiffies + msecs_to_jiffies(DELAY));
 }
 
 int init_module(void)
 {
 	printk(KERN_INFO "Hello, World !\n");
+
+	if(gpio_request(BUTTON, "No label")){
+		printk("Can't allocate GPIO %d\n", BUTTON);
+	}
+	
+	if(gpio_direction_input(BUTTON)){
+		printk("Can't set GPIO %d to output\n", BUTTON);
+	}
+
+		
 	timer_setup(&kernel_timer, timer_callback, NO_USE_FLAG);
 
 	kernel_timer.expires = jiffies + msecs_to_jiffies(DELAY); //Delay at DELAY seconds for the 1st time
