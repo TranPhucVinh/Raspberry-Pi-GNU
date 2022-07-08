9a34bceb14023e77aee8d12b122cca4f333a9cf7
Tran Phuc Vinh <tranphucvinh96@gmail.com>
2022-07-08 23:42:04 +0700

    Update interrupt example

--- Kernel/README.md
+++ Kernel/README.md
@@ -48,4 +48,8 @@ In 12th February 2022, with Raspbian installed from ``2021-10-30-raspios-bullsey
 # GPIO control
 
 * [blink_led.c](blink_led.c): Blink LED by GPIO kernel API and kernel timer setup
-* [control_led_with_button.c](control_led_with_button.c): Use button to control the LED: If button is pressed, turn on LED, if not pressed, turn off LED.
+* [control_led_with_button.c](control_led_with_button.c): Use button to control the LED. If button is pressed, turn on LED, if not pressed, turn off LED.
+
+# Interrupt
+
+* [toggle_led_by_interrupt.c](toggle_led_by_interrupt.c): Toggle LED status by pressing button with interrupt. If pressed button for the first time, LED turn on, press one more time, LED turn off.
---
+++ Kernel/toggle_led_by_interrupt.c
@@ -0,0 +1,49 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/gpio.h>
+#include <linux/interrupt.h>
+
+#define LED			3
+#define BUTTON		2
+
+#define DEV_NAME    "Device name"
+#define DEV_ID      NULL
+
+MODULE_LICENSE("GPL");
+
+int irq_number;
+int led_status;
+
+irq_handler_t gpio_irq_handler(unsigned int irq, void* dev_id, struct pt_regs *regs){
+	led_status = !led_status;
+	gpio_direction_output(LED, led_status);
+
+    return (irq_handler_t) IRQ_HANDLED;
+}
+
+int init_module(void)
+{
+	printk(KERN_INFO "Hello, World !\n");
+
+    if(gpio_direction_input(BUTTON)){
+        printk("Unable to set GPIO %d to input\n", BUTTON);
+    }
+
+	irq_number = gpio_to_irq(BUTTON);
+
+    if (request_irq(irq_number, (irq_handler_t) gpio_irq_handler, IRQF_TRIGGER_RISING, DEV_NAME, DEV_ID) != 0){
+        printk("Can't request interrupt number %d\n", irq_number);
+    } else printk("Request interrupt number %d successfully\n", irq_number);
+
+	return 0;
+}
+
+void cleanup_module(void)
+{
+    /*
+        Must have free_irq() function in cleanup (for rmmod command)
+        If not having free_irq(), the interrupt still existed in /proc/interrupts after calling rmmod
+    */
+	free_irq(irq_number, DEV_ID);
+	printk(KERN_INFO "clean up module\n");
+}
