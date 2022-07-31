47f29913d513c7759a34e355183f077506a8fca6
TranPhucVinh <30307543+TranPhucVinh@users.noreply.github.com>
2022-07-31 23:05:53 +0700

    Remove unused macro

--- Kernel/blink_led_with_ioremap.c
+++ Kernel/blink_led_with_ioremap.c
@@ -3,43 +3,18 @@
 #include <linux/kthread.h>
 #include <linux/delay.h> //For msleep()
 #include <linux/io.h>
-#include <asm/io.h>
 
 #define BCM2837_PHY_ADDR                0x3f000000 //BCM2837 physical address start
 #define GPIO_BASE                       (BCM2837_PHY_ADDR + 0x200000) /* GPIO controller */
 
-#define GPIO_INP(addr,pin) *(addr+((pin)/10)) &= ~(7<<(((pin)%10)*3))
 #define GPIO_OUT(addr,pin) {\
         *(addr+((pin)/10)) &= ~(7<<(((pin)%10)*3));\
         *(addr+((pin)/10)) |= (1<<(((pin)%10)*3));\
 }
 #define GPIO_SET(addr,pin) *(addr+7)=1<<pin //Set GPIO in GPSET0
 #define GPIO_CLR(addr,pin) *(addr+10)=1<<pin //Clear GPIO in GPCLR0
-#define GPIO_GET(addr,pin) ((*(addr+13))>>pin)&1 //GET GPIO value in GPLEV0
 
-// #define SET_GPIO_ALT(g,a) *(gpio+(((g)/10))) |= (((a)<=3?(a)+4:(a)==4?3:2)<<(((g)%10)*3))
-
-#define short_wait for(int ii=0;ii<=200;ii++)
-#define NO_PULL 0
-#define PULL_D 1
-#define PULL_U 2
-#define GPIO_PULL(addr,state,list_of_pin) \
-{\
-        int pin_array[] = list_of_pin;\
-        int nums = sizeof(pin_array)/sizeof(int);\
-        uint32_t pulld_reg = 0;\
-        for(int count = 0; count < nums; count++){\
-                pulld_reg |= 1 << pin_array[count];\
-        }\
-        *(addr+37) = state;\
-        short_wait;\
-        *(addr+38) = pulld_reg;\
-        short_wait;\
-        *(addr+37)=0;\
-        *(addr+38)=0;\
-}
-
-#define LED			4
+#define LED			3
 
 #define DELAY       1000
 
