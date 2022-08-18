e5026c8f2f3ae72f8edc1a46651254158bb014b4
TranPhucVinh <30307543+TranPhucVinh@users.noreply.github.com>
2022-08-18 04:34:24 -0400

    Add update_property_for_overlay_device_tree.c

---
+++ Kernel/Device tree/update_property_for_overlay_device_tree.c
@@ -0,0 +1,51 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/of.h>
+#include <linux/slab.h>
+
+MODULE_LICENSE("GPL");
+
+struct device_node  *dev_node;
+struct list_head    *lh;
+struct of_changeset *ocs;
+struct property  	*prop;
+
+struct mutex 		of_mutex;
+
+int init_module(void)
+{
+	//Find the overlay device node with device_type "new_dt_node"
+	dev_node = kmalloc(sizeof(struct device_node*), GFP_KERNEL);
+	dev_node = of_find_node_by_type(NULL, "new_dt_node");
+	
+	//Init list_head and of_changset objects
+	lh = kmalloc(sizeof(struct list_head*), GFP_KERNEL);
+	INIT_LIST_HEAD(lh);
+
+	ocs = kmalloc(sizeof(struct of_changeset*), GFP_KERNEL);
+	ocs->entries = *lh;
+	of_changeset_init(ocs);
+
+	prop = kmalloc(sizeof(struct property*), GFP_KERNEL);
+	prop->name = kstrdup("compatible", GFP_KERNEL);//property name
+	prop->value = kstrdup("new_compatible_string", GFP_KERNEL);//property value
+	prop->length = strlen(prop->value) + 1;
+
+	//Update the "compatible" property
+	of_changeset_update_property(ocs, dev_node, prop);
+
+	mutex_lock(&of_mutex);
+	int ret = of_changeset_apply(ocs);
+	if(ret < 0) {
+		printk("Unable to update device tree property, ret: %d\n", ret);
+		return -1;
+	}
+	mutex_unlock(&of_mutex);
+
+	return 0;
+}
+
+void cleanup_module(void)
+{
+	printk(KERN_INFO "clean up module\n");
+}
