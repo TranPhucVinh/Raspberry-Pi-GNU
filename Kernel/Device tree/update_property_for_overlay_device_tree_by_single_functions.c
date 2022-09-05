3936b0a37d953c0358e597f1a7a83fa2f2fe2139
Tran Phuc Vinh <tranphucvinh96@gmail.com>
2022-09-05 23:43:27 +0700

    Add related kmalloc() and kfree() functions

--- Kernel/Device tree/update_property_for_overlay_device_tree_by_single_functions.c
+++ Kernel/Device tree/update_property_for_overlay_device_tree_by_single_functions.c
@@ -3,16 +3,17 @@
 #include <linux/of.h>
 #include <linux/slab.h>
 
+DEFINE_MUTEX(of_mutex);
 MODULE_LICENSE("GPL");
 
 struct device_node  *dev_node;
 struct of_changeset *ocs;
 struct property  	*prop;
 
-struct mutex 		of_mutex;
-
 int init_module(void)
 {
+	mutex_init(&of_mutex);
+
 	//Find the overlay device node with device_type "new_dt_node"
 	dev_node = of_find_node_by_type(NULL, "new_dt_node");
 	
@@ -40,5 +41,8 @@ int init_module(void)
 
 void cleanup_module(void)
 {
+	of_changeset_revert(ocs);
+	kfree(prop);
+	kfree(ocs);
 	printk(KERN_INFO "clean up module\n");
 }
